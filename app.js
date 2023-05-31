3547e39cb0003119cdff068d7e67506d9d5e8d60
Gretil <gretil_15@hotmail.com>
2023-05-31 14:21:01 +0200

    Added some handlebars

--- app.js
+++ app.js
@@ -1,17 +1,63 @@
 /** @format */
 
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
 app.use(express.static("public")); /// make everything inside of public/ available
 
+app.set("views", __dirname + "/views"); //tells our Express app where to look for our views
+app.set("view engine", "hbs"); //sets HBS as the template engine
+
+hbs.registerPartials(__dirname + "/views/partials");
+
+/// GETS
+
 app.get("/", (req, res, next) => {
-	res.sendFile(__dirname + "/views/home-page.html");
+	// res.sendFile(__dirname + "/views/home-page.html");
+
+	res.render("home-page");
 });
 
 app.get("/contact", (req, res, next) => {
-	res.sendFile(__dirname + "/views/contact-page.html");
+	res.render("contact-page");
+});
+
+app.get("/pizzas/margarita", (req, res, next) => {
+	const pizzaDetails = {
+		name: "Margarita",
+		price: 12,
+		recommendedDrink: "beer",
+		imageFile: "pizza-margarita.jpg",
+		ingredients: ["mozzarella", "tomato sauce", "basilicum"],
+	};
+
+	res.render("product", pizzaDetails);
+});
+
+app.get("/pizzas/veggie", (req, res, next) => {
+	const pizzaDetails = {
+		name: "Veggie Pizza",
+		price: 15,
+		recommendedDrink: "power smoothie",
+		imageFile: "pizza-veggie.jpg",
+		ingredients: ["cherry tomatoes", "basilicum", "Olives"],
+	};
+
+	res.render("product", pizzaDetails);
+});
+
+app.get("/pizzas/seafood", (req, res, next) => {
+	const pizzaDetails = {
+		name: "Seafood Pizza",
+
+		recommendedDrink: "white wine",
+		imageFile: "pizza-seafood.jpg",
+		ingredients: ["tomato sauce", "garlic", "prawn"],
+	};
+
+	res.render("product", pizzaDetails);
 });
 
 app.listen(3000, () => {
